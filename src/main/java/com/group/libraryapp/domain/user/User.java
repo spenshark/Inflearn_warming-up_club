74b2307e4392f94fe63af3c2f11776a5c832ac1a
spenshark <xzx01299@naver.com>
2024-03-08 01:51:39 +0900

    민감한 정보 숨김

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,12 +1,15 @@
 package com.group.libraryapp.domain.user;
 
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id
@@ -18,10 +21,8 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserLoanHistory> userLoanHistories = new ArrayList<>();
 
-    protected User() {}
-
     public User(String name, Integer age) {
-        if(name == null || name.isBlank()){
+        if (name == null || name.isBlank()) {
             throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다", name));
         }
         this.name = name;
@@ -40,7 +41,7 @@ public class User {
         return id;
     }
 
-    public void updateName(String name){
+    public void updateName(String name) {
         this.name = name;
     }
 
